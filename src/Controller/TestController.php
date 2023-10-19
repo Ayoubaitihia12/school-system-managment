1623d442b90fc80592a29db5cf9005c48fac2e44
ayoub <ayoubaitihia@gmail.com>
2023-10-19 11:57:46 +0200

    'V.19.10.23.11.57'

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -5,24 +5,29 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Polyfill\Intl\Icu\Locale;
+use Symfony\Component\Translation\LocaleSwitcher;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 class TestController extends AbstractController
 {
-    public function index(Request $request): Response
+    public function __construct(
+        private LocaleSwitcher $localeSwitcher,
+    ) {
+    }
+    public function index(Request $request , TranslatorInterface $translator , LocaleSwitcher $localeSwitcher): Response
     {
-        Locale::setDefault('en_US');
         
-        $language = $request->getLocale();
+        // $this->localeSwitcher->setLocale('fr');
 
-        if ($language === 'en') {
-            $request->setLocale('fr');
-        } elseif ($language === 'fr') {
-            $request->setLocale('en');
-        }
-
-        // dd($language);
+        // dd($greeting);
     
         return $this->render('test/index.html.twig');
+    }
+
+    public function changeLocale(Request $request, $_locale): Response
+    {
+        $this->localeSwitcher->setLocale($_locale);
+
+        return $this->redirect($request->headers->get('referer'));
     }
 }
